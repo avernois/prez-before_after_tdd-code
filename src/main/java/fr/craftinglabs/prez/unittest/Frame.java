4c60b6394ef8ecfe64f1f1909011f46ac1c9d23f
Antoine <antoine@crafting-labs.fr>
2014-10-15 17:43:25 +0200

    add an implementation found on github https://github.com/ihaworth/BowlingGameKata

---
+++ src/main/java/fr/craftinglabs/prez/unittest/Frame.java
@@ -0,0 +1,102 @@
+package fr.craftinglabs.prez.unittest;
+
+
+public class Frame
+{
+    private static final int MAX_ROLLS = 2;
+    private static final int BONUS_WORTHY_SCORE = 10;
+
+    protected int[] rolls;
+    private int currentRoll = 0;
+
+    private Frame nextFrame;
+
+    public Frame()
+    {
+        init();
+    }
+
+    private void init()
+    {
+        rolls = new int[maxRolls()];
+    }
+
+    public void setNextFrame(Frame nextFrame)
+    {
+        this.nextFrame = nextFrame;
+    }
+
+    public void roll(int pins)
+    {
+        rolls[currentRoll] = pins;
+        currentRoll++;
+    }
+
+    public int scoreWithBouns()
+    {
+        if (isStrike())
+            return firstRoll() + nextFrame.strikeBonusForPreviousFrame();
+        else if (isSpare())
+            return frameTotal() + nextFrame.spareBonusForPreviousFrame();
+        else
+            return frameTotal();
+    }
+
+    int strikeBonusForPreviousFrame()
+    {
+        if (isStrike())
+            return firstRoll() + nextFrame.firstRoll();
+        else
+            return frameTotal();
+    }
+
+    private int frameTotal()
+    {
+        return firstRoll() + secondRoll();
+    }
+
+    int spareBonusForPreviousFrame()
+    {
+        return firstRoll();
+    }
+
+    protected int firstRoll()
+    {
+        return rolls[0];
+    }
+
+    protected int secondRoll()
+    {
+        return rolls[1];
+    }
+
+    public boolean isOver()
+    {
+        return isStrike() || allRollsHaveBeenMade();
+    }
+
+    protected boolean allRollsHaveBeenMade()
+    {
+        return currentRoll == maxRolls();
+    }
+
+    protected boolean twoRollsHaveBeenMade()
+    {
+        return currentRoll == 2;
+    }
+
+    protected int maxRolls()
+    {
+        return MAX_ROLLS;
+    }
+
+    public boolean isSpare()
+    {
+        return !isStrike() && frameTotal() == BONUS_WORTHY_SCORE;
+    }
+
+    public boolean isStrike()
+    {
+        return firstRoll() == BONUS_WORTHY_SCORE;
+    }
+}
---
+++ src/main/java/fr/craftinglabs/prez/unittest/LastFrame.java
@@ -0,0 +1,37 @@
+package fr.craftinglabs.prez.unittest;
+
+public class LastFrame extends Frame
+{
+    private static final int MAX_ROLLS = 3;
+
+    @Override
+    public boolean isOver()
+    {
+        return isStrike() && allRollsHaveBeenMade() ||
+                isSpare() && allRollsHaveBeenMade() ||
+                !isStrike() && !isSpare() && twoRollsHaveBeenMade();
+    }
+
+    @Override
+    protected int maxRolls()
+    {
+        return MAX_ROLLS;
+    }
+
+    @Override
+    int strikeBonusForPreviousFrame()
+    {
+        return firstRoll() + secondRoll();
+    }
+
+    @Override
+    public int scoreWithBouns()
+    {
+        return firstRoll() + secondRoll() + thirdRoll();
+    }
+
+    private int thirdRoll()
+    {
+        return rolls[2];
+    }
+}
